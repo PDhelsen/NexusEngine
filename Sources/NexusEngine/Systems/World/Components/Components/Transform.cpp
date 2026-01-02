b737c5fc0c0b089310dadce364d4a09733d5f49c
PierreVandhelsen <vandhelsen.pierre@gmail.com>
2026-01-02 18:19:06 -0500

    [World] Transform

--- Sources/NexusEngine/Systems/World/Components/Components/Transform.cpp
+++ Sources/NexusEngine/Systems/World/Components/Components/Transform.cpp
@@ -5,13 +5,205 @@ namespace NxEn
 {
 	NEXUS_COMPONENT_IMPLEMENTATION(Transform)
 
+	NxFr::Vector3f Transform::TransformPosition(const NxFr::Matrix4x4f& Space, NxFr::Vector3f Position)
+	{
+		return Space * NxFr::Vector4f(Position.x, Position.y, Position.z, 1.0f);
+	}
+
+	NxFr::Vector3f Transform::TransformVector(const NxFr::Matrix4x4f& Space, NxFr::Vector3f Vector)
+	{
+		return Space * NxFr::Vector4f(Vector.x, Vector.y, Vector.z, 0.0f);
+	}
+
+	NxFr::Vector3f Transform::TransformDirection(const NxFr::Matrix4x4f& Space, NxFr::Vector3f Direction)
+	{
+		return Space.GetRotation() * NxFr::Vector4f(Direction.x, Direction.y, Direction.z, 0.0f);
+	}
+
+	NxFr::Quaternion Transform::TransformRotation(const NxFr::Matrix4x4f& Space, NxFr::Quaternion Rotation)
+	{
+		return Space.GetRotation() * Rotation;
+	}
+
 	Transform::Transform()
-		: Position(), Rotation(), Scale()
+		: Position(NxFr::Vector3f::Zero), Rotation(NxFr::Quaternion::Identity), Scaling(NxFr::Vector3f::One)
 	{
 	}
 
 	Transform::~Transform()
 	{
+	}
+
+	void Transform::Translate(NxFr::Vector3f Delta, TransformSpace Space)
+	{
+		switch (Space)
+		{
+		case NxEn::TransformSpace::World: Position += Delta; return;
+		case NxEn::TransformSpace::Local: Position += Rotation * Delta; return;
+		}
+	}
+
+	void Transform::Rotate(NxFr::Quaternion Delta, TransformSpace Space)
+	{
+		switch (Space)
+		{
+		case NxEn::TransformSpace::World: Rotation = Rotation * Delta; return;
+		case NxEn::TransformSpace::Local: Rotation = Delta * Rotation; return;
+		}
+	}
+
+	void Transform::Scale(NxFr::Vector3f Factor, TransformSpace Space)
+	{
+		Scaling *= Factor;
+	}
+
+	void Transform::LookAtPosition(NxFr::Vector3f Target, NxFr::Vector3f Up, TransformSpace Space)
+	{
+		NxFr::Handle<Transform> Parent = GetParent();
+		if (Space == TransformSpace::Local && Parent)
+		{
+			Target = Parent->LocalToWorld() * NxFr::Vector4f(Target.x, Target.y, Target.z, 1.0f);
+		}
+
+		NxFr::Vector3f Direction = NxFr::VectorUtility::Normalize(Target - GetPosition(TransformSpace::World));
+		Rotation = NxFr::RotationUtility::LookAt(Direction, Up);
+	}
+
+	void Transform::LookAtDirection(NxFr::Vector3f Direction, NxFr::Vector3f Up, TransformSpace Space)
+	{
+		NxFr::Handle<Transform> Parent = GetParent();
+		if (Space == TransformSpace::Local && Parent)
+		{
+			Direction = Parent->LocalToWorld() * NxFr::Vector4f(Direction.x, Direction.y, Direction.z, 0.0f);
+		}
+
+		Direction = NxFr::VectorUtility::Normalize(Direction);
+		Rotation = NxFr::RotationUtility::LookAt(Direction, Up);
+	}
+
+	NxFr::Vector3f Transform::GetPosition(TransformSpace Space) const
+	{
+		NxFr::Vector3f Result = Position;
+
+		NxFr::Handle<Transform> Parent = GetParent();
+		if (Space == TransformSpace::World && Parent)
+		{
+			Result = Parent->GetPosition(TransformSpace::World) + Transform::TransformPosition(Parent->LocalToWorld(), Position);
+		}
+
+		return Result;
+	}
+
+	void Transform::SetPosition(NxFr::Vector3f Position, TransformSpace Space)
+	{
+		NxFr::Handle<Transform> Parent = GetParent();
+		if (Space == TransformSpace::World && Parent)
+		{
+			Position = Transform::TransformPosition(Parent->WorldToLocal(), Position - Parent->GetPosition(Space));
+		}
+
+		this->Position = Position;
+	}
+
+	NxFr::Quaternion Transform::GetRotation(TransformSpace Space) const
+	{
+		NxFr::Quaternion Result = Rotation;
+
+		NxFr::Handle<Transform> Parent = GetParent();
+		if (Space == TransformSpace::World && Parent)
+		{
+			Result = Parent->GetRotation(Space) * Result;
+		}
+
+		return Result;
+	}
+
+	void Transform::SetRotation(NxFr::Quaternion Rotation, TransformSpace Space)
+	{
+		NxFr::Handle<Transform> Parent = GetParent();
+		if (Space == TransformSpace::World && Parent)
+		{
+			Rotation = Parent->GetRotation(Space).Inverse() * Rotation;
+		}
+
+		this->Rotation = Rotation;
+	}
+
+	NxFr::Vector3f Transform::GetScale(TransformSpace Space) const
+	{
+		NxFr::Vector3f Result = Scaling;
+
+		NxFr::Handle<Transform> Parent = GetParent();
+		if (Space == TransformSpace::World && Parent)
+		{
+			Result *= Parent->GetScale(Space);
+		}
+
+		return Result;
+	}
+
+	void Transform::SetScale(NxFr::Vector3f Scale, TransformSpace Space)
+	{
+		NxFr::Handle<Transform> Parent = GetParent();
+		if (Space == TransformSpace::World && Parent)
+		{
+			Scale /= Parent->GetScale(Space);
+		}
+
+		this->Scaling = Scale;
+	}
+
+	NxFr::Matrix4x4f Transform::GetMatrix() const
+	{
+		return NxFr::Matrix4x4f::TRS(Position, Rotation, Scaling);
+	}
+
+	void Transform::SetMatrix(NxFr::Matrix4x4f Matrix)
+	{
+		Position = Matrix.GetPosition();
+		Rotation = Matrix.GetRotation();
+		Scaling = Matrix.GetScale();
+	}
+
+	NxFr::Vector3f Transform::Right(TransformSpace Space) const
+	{
+		return GetRotation(Space) * NxFr::Vector3f::Right;
+	}
+
+	NxFr::Vector3f Transform::Up(TransformSpace Space) const
+	{
+		return GetRotation(Space) * NxFr::Vector3f::Up;
+	}
+
+	NxFr::Vector3f Transform::Forward(TransformSpace Space) const
+	{
+		return GetRotation(Space) * NxFr::Vector3f::Forward;
+	}
+
+	NxFr::Matrix4x4f Transform::LocalToWorld() const
+	{
+		NxFr::Matrix4x4f Result = GetMatrix();
+
+		NxFr::Handle<Transform> Parent = GetParent();
+		if (Parent)
+		{
+			return Parent->LocalToWorld() * Result;
+		}
+
+		return Result;
+	}
+
+	NxFr::Matrix4x4f Transform::WorldToLocal() const
+	{
+		NxFr::Matrix4x4f Result = GetMatrix().Inverse();
+
+		NxFr::Handle<Transform> Parent = GetParent();
+		if (Parent)
+		{
+			return Result * Parent->WorldToLocal();
+		}
+
+		return Result;
 	}
 
 	void Transform::OnGui(float TimeStep)
@@ -20,7 +212,7 @@ namespace NxEn
 
 		GUI::Drawer<NxFr::Vector3f>::Field(Position, "Position");
 		GUI::Drawer<NxFr::Quaternion>::Field(Rotation, "Rotation");
-		GUI::Drawer<NxFr::Vector3f>::Field(Scale, "Scale");
+		GUI::Drawer<NxFr::Vector3f>::Field(Scaling, "Scale");
 	}
 
 	void Transform::OnClone(const Object& Other)
@@ -30,7 +222,7 @@ namespace NxEn
 
 		Position = Instance.Position;
 		Rotation = Instance.Rotation;
-		Scale = Instance.Scale;
+		Scaling = Instance.Scaling;
 	}
 
 	void Transform::OnSave(YAML::Node& Node)
@@ -39,7 +231,7 @@ namespace NxEn
 
 		Node["Position"] = Position;
 		Node["Rotation"] = Rotation;
-		Node["Scale"] = Scale;
+		Node["Scaling"] = Scaling;
 	}
 
 	void Transform::OnLoad(const YAML::Node& Node)
@@ -48,6 +240,11 @@ namespace NxEn
 
 		Position = Node["Position"].as<NxFr::Vector3f>();
 		Rotation = Node["Rotation"].as<NxFr::Quaternion>();
-		Scale = Node["Scale"].as<NxFr::Vector3f>();
+		Scaling = Node["Scaling"].as<NxFr::Vector3f>();
+	}
+
+	NxFr::Handle<Transform> Transform::GetParent() const
+	{
+		return GetGameObject()->GetParent()->GetComponent<Transform>();
 	}
 }
--- Sources/NexusEngine/Systems/World/Components/Components/Transform.h
+++ Sources/NexusEngine/Systems/World/Components/Components/Transform.h
@@ -4,13 +4,45 @@
 
 namespace NxEn
 {
+	enum class TransformSpace
+	{
+		World, Local
+	};
+
 	class Transform : public Component
 	{
 	public:
+		NEXUS_ENGINE_API static NxFr::Vector3f TransformPosition(const NxFr::Matrix4x4f& Space, NxFr::Vector3f Position);
+		NEXUS_ENGINE_API static NxFr::Vector3f TransformVector(const NxFr::Matrix4x4f& Space, NxFr::Vector3f Vector);
+		NEXUS_ENGINE_API static NxFr::Vector3f TransformDirection(const NxFr::Matrix4x4f& Space, NxFr::Vector3f Direction);
+		NEXUS_ENGINE_API static NxFr::Quaternion TransformRotation(const NxFr::Matrix4x4f& Space, NxFr::Quaternion Rotation);
+
 		NEXUS_COMPONENT_DECLARATION(NEXUS_ENGINE_API, Transform)
 
 		NEXUS_ENGINE_API Transform();
 		NEXUS_ENGINE_API ~Transform();
+
+		NEXUS_ENGINE_API void Translate(NxFr::Vector3f Delta, TransformSpace Space = TransformSpace::World);
+		NEXUS_ENGINE_API void Rotate(NxFr::Quaternion Delta, TransformSpace Space = TransformSpace::World);
+		NEXUS_ENGINE_API void Scale(NxFr::Vector3f Factor, TransformSpace Space = TransformSpace::World);
+		NEXUS_ENGINE_API void LookAtPosition(NxFr::Vector3f Target, NxFr::Vector3f Up = NxFr::Vector3f::Up, TransformSpace Space = TransformSpace::World);
+		NEXUS_ENGINE_API void LookAtDirection(NxFr::Vector3f Direction, NxFr::Vector3f Up = NxFr::Vector3f::Up, TransformSpace Space = TransformSpace::World);
+
+		NEXUS_ENGINE_API NxFr::Vector3f GetPosition(TransformSpace Space = TransformSpace::World) const;
+		NEXUS_ENGINE_API void SetPosition(NxFr::Vector3f Position, TransformSpace Space = TransformSpace::World);
+		NEXUS_ENGINE_API NxFr::Quaternion GetRotation(TransformSpace Space = TransformSpace::World) const;
+		NEXUS_ENGINE_API void SetRotation(NxFr::Quaternion Rotation, TransformSpace Space = TransformSpace::World);
+		NEXUS_ENGINE_API NxFr::Vector3f GetScale(TransformSpace Space = TransformSpace::World) const;
+		NEXUS_ENGINE_API void SetScale(NxFr::Vector3f Scale, TransformSpace Space = TransformSpace::World);
+		NEXUS_ENGINE_API NxFr::Matrix4x4f GetMatrix() const;
+		NEXUS_ENGINE_API void SetMatrix(NxFr::Matrix4x4f Matrix);
+
+		NEXUS_ENGINE_API NxFr::Vector3f Right(TransformSpace Space = TransformSpace::World) const;
+		NEXUS_ENGINE_API NxFr::Vector3f Up(TransformSpace Space = TransformSpace::World) const;
+		NEXUS_ENGINE_API NxFr::Vector3f Forward(TransformSpace Space = TransformSpace::World) const;
+
+		NEXUS_ENGINE_API NxFr::Matrix4x4f LocalToWorld() const;
+		NEXUS_ENGINE_API NxFr::Matrix4x4f WorldToLocal() const;
 
 	protected:
 		NEXUS_ENGINE_API void OnGui(float TimeStep) override;
@@ -19,9 +51,12 @@ namespace NxEn
 		NEXUS_ENGINE_API void OnLoad(const YAML::Node& Node) override;
 
 	private:
+		NEXUS_ENGINE_API NxFr::Handle<Transform> GetParent() const;
+
+	private:
 		NxFr::Vector3f Position;
 		NxFr::Quaternion Rotation;
-		NxFr::Vector3f Scale;
+		NxFr::Vector3f Scaling;
 	};
 }
 
